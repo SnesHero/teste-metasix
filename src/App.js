a3b56a6e9c10c5c213b6c5b6395df85361bc398a
SnesHero <jojo.telhada@gmail.com>
2021-03-01 21:22:04 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./images/logo.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App-azul">
+        <img src={logo} />
+      </div>
+
+      <div>
+        <button>HOME</button>
+        <button>FAQ</button>
+        <button>LOCAIS</button>
+        <button>CATEGORIA</button>
+      </div>
+
+      <div>
+        <h1>FAQ - Perguntas Frequentes</h1>
+        <input></input>
+      </div>
+      
     </div>
   );
 }

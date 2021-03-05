f2aae3c6fe64ec3ecad0b426912d7478e3b4d1a4
SnesHero <jojo.telhada@gmail.com>
2021-03-05 15:53:23 -0300

    Adds some CSS

--- src/FAQ/FAQ_perguntas.js
+++ src/FAQ/FAQ_perguntas.js
@@ -4,7 +4,7 @@ import "./FAQ";
 import useFAQ_perguntas from "./useFAQ_perguntas";
 import Axios from "axios";
 
-function FAQ_perguntas({texto, ordem, resposta,}) {
+function FAQ_perguntas({ texto, ordem, resposta }) {
   const [toggle, setToggle] = useState(false);
 
   const mostra_resposta = () => {
@@ -14,10 +14,14 @@ function FAQ_perguntas({texto, ordem, resposta,}) {
   return (
     <div>
       <div className="Faq-perguntas">
-        <div onClick={mostra_resposta}>{texto}</div>
-        <div>{ordem}</div>
-        <button>Editar</button>
-        <button>Excluir</button>
+        <div onClick={mostra_resposta} className="Titulo-pergunta">
+          {texto}
+        </div>
+        <div className="Icones">
+          <div className="Icon-item">{ordem}</div>
+          <button className="Icon-item">Editar</button>
+          <button className="Icon-item">Excluir</button>
+        </div>
       </div>
       <div>{toggle ? <input value={resposta}></input> : null}</div>
     </div>
